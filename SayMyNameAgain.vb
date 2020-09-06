c76f8fc010ee2725a0d1a97617cb503196689aac
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-06 12:12:35 -0600

    emily and Joe

--- SayMyNameAgain.vb
+++ SayMyNameAgain.vb
@@ -1,12 +1,38 @@
-﻿Module SayMyNameAgain
+﻿
+Option Strict On
+Option Explicit On
+Option Compare Text
 
-    Sub Main()
+'Justine Hffa
+'RCET0265
+'Fall 2020
+'Say My Name Again
+'https://github.com/justinehoffa/SayMyNameAgain
 
+Module SayMyNameAgain
 
+        Sub Main()
+            Dim userInput As String
 
+            Console.WriteLine("What is your name?")
+            userInput = Console.ReadLine()
 
+        If userInput = "Emily" Then
+            Console.WriteLine("Hi Emily.")
 
+        ElseIf userInput = "Joe" Then
+            Console.WriteLine("Hi, Joe.")
 
-    End Sub
+        ElseIf userInput = "Justine" Then
+            Console.WriteLine("ew")
 
-End Module
+        ElseIf userInput = "Tim" Then
+            Console.WriteLine("Hi, Tim!")
+
+        End If
+
+            Console.ReadLine()
+
+        End Sub
+    End Module
+
